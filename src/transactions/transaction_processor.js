0d1fcd0a4d0efe3fdcb3cd44dcfae493c77b221d
Beanalini <dat826@gmail.com>
2023-10-25 14:22:36 +0100

    refactor: removed sortByAmountThenName function.
    Put sort by value and key code into processTransactions function and chained with map method to return array in required format

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -9,34 +9,14 @@ function processTransactions(transActions) {
     return { ...transaction, [entry]: (transaction[entry] || 0) + 1 };
   }, {});
 
-  txCount = sortByAmountThenName(txCount);
-
-  // Place them back in array for returning
-  Object.keys(txCount).forEach(function (key, index) {
-    txr[index] = `${key} ${txCount[key]}`;
-  });
-
-  return txr;
-}
-
-function sortByAmountThenName(txCount) {
-  let sortedKeys = Object.keys(txCount).sort(function sortingFunction(
-    itemOne,
-    itemTwo
-  ) {
-    return (
-      txCount[itemTwo] - txCount[itemOne] ||
-      itemOne > itemTwo ||
-      -(itemOne < itemTwo)
-    );
-  });
-
-  let sortedResults = {};
-  for (let objectKey of sortedKeys) {
-    sortedResults[objectKey] = txCount[objectKey];
-  }
-
-  return sortedResults;
+  return Object.keys(txCount)
+    .sort(
+      (itemOne, itemTwo) =>
+        txCount[itemTwo] - txCount[itemOne] ||
+        itemOne > itemTwo ||
+        -(itemOne < itemTwo)
+    )
+    .map((key) => `${key} ${txCount[key]}`);
 }
 
 module.exports = processTransactions;
